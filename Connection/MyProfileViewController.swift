1e8c0ae8ab018dd539a30484ea586172b72d8a95
shi-edward <shi_edward@icloud.com>
2016-08-03 23:35:09 -0500

    profile

---
+++ Connection/MyProfileViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MyProfileViewController.swift
+//  Connection
+//
+//  Created by Edward on 8/3/16.
+//  Copyright © 2016 Edward. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+class MyProfileViewController: UIViewController {
+
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var addressLabel: UILabel!
+    // username is phone number
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        email.text = FIRAuth.auth()?.currentUser?.email
+        DataService.dataService.CURRENT_USER_REF.child("username").observeSingleEventOfType(.Value, withBlock: { snapchat in
+            let snap = snapchat.value
+            self.phoneNumber.text = snap as? String
+        })
+        DataService.dataService.CURRENT_USER_REF.child("password").observeSingleEventOfType(.Value, withBlock: { snapchat in
+            let snap = snapchat.value
+            self.password.text = snap as? String
+        })
+        address.hidden = true
+        addressLabel.hidden = true
+        DataService.dataService.CURRENT_USER_REF.child("status").observeSingleEventOfType(.Value, withBlock: { snapshot in
+            if let snap = snapshot.value {
+                if snap as! String == "host"{
+                    self.address.hidden = false
+                    self.addressLabel.hidden = false
+                    DataService.dataService.CURRENT_USER_REF.child("address").observeSingleEventOfType(.Value, withBlock: { snapchat in
+                        let snap = snapchat.value
+                        self.address.text = snap as? String
+                    })
+                }
+            }
+            })
+        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func saveProfileButton(sender: AnyObject) {
+    }
+}
